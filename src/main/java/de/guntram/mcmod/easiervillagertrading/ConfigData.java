4d3437088d9b8573fbe71bb6393fbc0f65b05b06
Main_User <Main_User@DESKTOP-AOSAKKM.mshome.net>
2023-09-02 15:38:33 +0900

    開発環境再構成とCTRLシフト対応

---
+++ src/main/java/de/guntram/mcmod/easiervillagertrading/ConfigData.java
@@ -0,0 +1,39 @@
+package de.guntram.mcmod.easiervillagertrading;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class ConfigData {
+
+    public static final ClientConfig CLIENT;
+    public static final ForgeConfigSpec CLIENT_SPEC;
+    public static boolean shiftSwapped;
+    public static boolean ctrlSwapped;
+
+    static {
+        final Pair<ClientConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ClientConfig::new);
+        CLIENT_SPEC = specPair.getRight();
+        CLIENT = specPair.getLeft();
+    }
+
+    public static void refreshClient() {
+        ctrlSwapped = CLIENT.shiftSwapped.get();
+        shiftSwapped = CLIENT.shiftSwapped.get();
+    }
+
+    public static class ClientConfig {
+
+        public final ForgeConfigSpec.BooleanValue shiftSwapped;
+        public final ForgeConfigSpec.BooleanValue ctrlSwapped;
+
+        ClientConfig(ForgeConfigSpec.Builder builder) {
+
+            builder.push("general");
+            shiftSwapped = builder.comment("Make 'trade all' default").translation("easiervillagertrading.config.tt.swapshift").define("shiftSwapped", false);
+            ctrlSwapped = builder.comment("Make 'trade Immediatry' default").translation("easiervillagertrading.config.tt.ctrlshift").define("ctrlSwapped", false);
+            builder.pop();
+
+        }
+    }
+}
